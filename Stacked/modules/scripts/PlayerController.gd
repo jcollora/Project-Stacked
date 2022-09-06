6acd5437970e2389d0e9e74c64627ddc251469d1
Bance40 <92686698+Bance40@users.noreply.github.com>
2022-09-06 12:01:14 -0700

    Meeting 1
    Jumping, crouching

--- Stacked/modules/scripts/PlayerController.gd
+++ Stacked/modules/scripts/PlayerController.gd
@@ -1,48 +1,98 @@
 extends KinematicBody
 
+var fov = 90
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+const ACCEL_DEFAULT = 7
+const ACCEL_AIR = 1
+onready var accel = ACCEL_DEFAULT
+var gravity = 9.8
+var default_height = 10
 
-var sensitivity = 0.03
-var speed = 10
+var cam_accel = 40
+var mouse_sense = 0.1
+var snap
+
+var speed = 7
+var jump = 5
+var sprint_multiplier = 3
+var crouching_speed = 20
+var crouch_multiplier = 0.6
+var crouch_height = 0.5
+
 var direction = Vector3()
+var velocity = Vector3()
+var gravity_vec = Vector3()
+var movement = Vector3()
 
-# $ helps obtain objects in the scene, onready to define vars using gameobject
-onready var head = $Head 
+onready var head = $Head
+onready var camera = $Head/Camera
+onready var pcap = $CollisionShape
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	# tells the engine to not display mouse when capturing input
+	#hides the cursor
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
 func _input(event):
+	#get mouse input for camera rotation
 	if event is InputEventMouseMotion:
-		# apply sensitivity
-		rotate_y(deg2rad(-event.relative.x * sensitivity))
-		head.rotate_x(deg2rad(-event.relative.y * sensitivity))
+		rotate_y(deg2rad(-event.relative.x * mouse_sense))
+		head.rotate_x(deg2rad(-event.relative.y * mouse_sense))
 		head.rotation.x = clamp(head.rotation.x, deg2rad(-89), deg2rad(89))
+
+func _process(delta):
+	#camera physics interpolation to reduce physics jitter on high refresh-rate monitors
+	if Engine.get_frames_per_second() > Engine.iterations_per_second:
+		camera.set_as_toplevel(true)
+		camera.global_transform.origin = camera.global_transform.origin.linear_interpolate(head.global_transform.origin, cam_accel * delta)
+		camera.rotation.y = rotation.y
+		camera.rotation.x = head.rotation.x
+	else:
+		camera.set_as_toplevel(false)
+		camera.global_transform = head.global_transform
 		
 func _physics_process(delta):
+	#get keyboard input
+	direction = Vector3.ZERO
+	var h_rot = global_transform.basis.get_euler().y
+	var f_input = Input.get_action_strength("move_backward") - Input.get_action_strength("move_forward")
+	var h_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	
-	direction = Vector3()
-	if Input.is_action_pressed("move_forward"):
-		direction -= transform.basis.z
-	elif Input.is_action_pressed("move_backward"):
-		direction += transform.basis.z
-	elif Input.is_action_pressed("move_left"):
-		direction -= transform.basis.x
-	elif Input.is_action_pressed("move_right"):
-		direction += transform.basis.x
-	direction = direction.normalized()
-	# moves object in direction vector by speed, up is a vector to define relative movement
-	move_and_slide(direction * speed, Vector3.UP)
+	direction = Vector3(h_input, 0, f_input).rotated(Vector3.UP, h_rot).normalized()
 	
+	#jumping and gravity
+	if is_on_floor():
+		snap = -get_floor_normal()
+		accel = ACCEL_DEFAULT
+		gravity_vec = Vector3.ZERO
+	else:
+		snap = Vector3.DOWN
+		accel = ACCEL_AIR
+		gravity_vec += Vector3.DOWN * gravity * delta
 		
+	if Input.is_action_just_pressed("move_jump") and is_on_floor():
+		snap = Vector3.ZERO
+		gravity_vec = Vector3.UP * jump
+	
+	## crouching
+	var speed_mult = 1
+	if Input.is_action_pressed("move_crouch") and !Input.is_action_pressed("move_sprint"):
+		pcap.shape.height -= crouching_speed * delta
+		speed_mult = crouch_multiplier
+	else:
+		pcap.shape.height += crouching_speed * delta
+		
+	pcap.shape.height = clamp(pcap.shape.height, crouch_height, default_height)
+	
+	## sprinting
+	if Input.get_action_strength("move_sprint") and Input.get_action_strength("move_forward") and !Input.get_action_strength("move_crouch"):
+		speed_mult = sprint_multiplier
+	
+	## movement calculation
+	var move_multiply = Vector3(1, 1, speed_mult)
+	velocity = velocity.linear_interpolate(direction * speed * move_multiply, accel * delta)
+	move_and_slide_with_snap(movement, snap, Vector3.UP)
+	movement = velocity + gravity_vec
+
+	var new_fov = fov * speed_mult
+	var t = 0.75
+	camera.fov = camera.fov * (new_fov - camera.fov) * t
